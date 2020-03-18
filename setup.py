6a8047d72c3d9701e63c2fb22dba120903dcb25a
DEMcKnight <dem@ou.edu>
2020-03-17 22:18:35 -0500

    Updating setup.py to be more modern, in accordance with python.org's recommendations

--- setup.py
+++ setup.py
@@ -1,10 +1,23 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name = 'elementally',
-      version = '0.2',
-      description = 'Utility class for elementwise operations on built-in types',
-      url = 'https://github.com/dem1995/elementally',
-      author = 'David "Dawn" Estes McKnight',
-      author_email = 'demcknig@ualberta.ca',
-	  license = 'MIT/X11',
-      packages = ['elementally'])
+with open("README.md", 'r') as readme:
+	long_description = readme.read()
+
+setup(
+	name = 'elementally-DEMcKnight',
+	version = '0.2',
+	author = 'David "Dawn" Estes McKnight',
+	author_email = 'demcknig@ualberta.ca',
+	description = 'Utility class for elementwise operations on built-in types',
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url = 'https://github.com/dem1995/elementally',
+	license = 'MIT/X11',
+	packages = find_packages(),
+	classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT/X11 License",
+        "Operating System :: OS Independent",
+    ],
+	python_requires='>=3.8'
+)
