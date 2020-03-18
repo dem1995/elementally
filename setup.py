cfc4861ea49df1ddee2d1c8673dfb9340c977a12
DEMcKnight <dem@ou.edu>
2020-03-17 21:55:48 -0500

    Un-excluding setup.py from github

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name = 'elementally',
+      version = '0.1',
+      description = 'Utility class for elementwise operations on built-in types',
+      url = 'https://github.com/dem1995/elementally',
+      author = 'David "Dawn" Estes McKnight',
+      author_email = 'demcknig@ualberta.ca',
+	  license = 'MIT/X11',
+      packages = ['elementally'])
